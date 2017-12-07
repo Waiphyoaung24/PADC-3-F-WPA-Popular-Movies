2c1e5ff21a0fe18dcb968d33f8273919c8d180ae
Wai Phyo Aung <waiphyoag.cs34@gmail.com>
2017-12-07 19:04:42 +0630

    Movie Shelf Adapter-based-View complete

---
+++ app/src/main/java/PopularMovieApp.java
@@ -0,0 +1,8 @@
+import android.app.Application;
+
+/**
+ * Created by WaiPhyoAg on 12/7/17.
+ */
+
+public class PopularMovieApp extends Application {
+}
---
+++ app/src/main/java/xyz/waiphyoag/padc_3_f_wpa_popular_movies/activities/activities/adapters/MoviesAdapter.java
@@ -0,0 +1,35 @@
+package xyz.waiphyoag.padc_3_f_wpa_popular_movies.activities.activities.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import xyz.waiphyoag.padc_3_f_wpa_popular_movies.R;
+import xyz.waiphyoag.padc_3_f_wpa_popular_movies.activities.activities.viewholders.ItemsViewsHolder;
+
+/**
+ * Created by WaiPhyoAg on 12/7/17.
+ */
+
+public class MoviesAdapter extends RecyclerView.Adapter {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View movieitemsView=inflater.inflate(R.layout.item_movies,parent,false);
+        ItemsViewsHolder itemsViewsHolder=new ItemsViewsHolder(movieitemsView);
+        return itemsViewsHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 6;
+    }
+}
